a04ce049addf20a579372dfb3e86b849d002d7a2
Stanley Lu <stanlehl@uci.edu>
2024-05-09 01:17:45 -0700

    Added more functionality for filereader, minus tokenization.

--- FileReader.py
+++ FileReader.py
@@ -1,5 +1,7 @@
+from collections import Counter
+
 from bs4 import BeautifulSoup
-import Posting
+import Posting as p
 import zipfile
 
 class FileReader:
@@ -12,12 +14,22 @@ class FileReader:
         index = {}
         # beginning of serialization of documents
         n = 0
-        # loop through all documents and tokenize content
         with zipfile.ZipFile(path, 'r') as files:
+            # loop through all documents and tokenize content
             for file in files.namelist():
                 with files.open(file) as doc:
-                    contents = doc.read()
+                    contents = doc.read().decode('utf-8')
                     n += 1
+                    soup = BeautifulSoup(contents, 'html.parser')
+                    text = soup.get_text() if soup else contents
+                    # replace this with tokenizer
+                    tokens = text.split()
+                    token_freq = Counter(tokens)
+                    for token, freq in token_freq.items():
+                        if token not in index:
+                            #create token in index and create posting object
+                            posting = p.Posting(n, freq)
+                            index[token] = posting
 
 
         return index
