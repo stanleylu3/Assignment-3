bf6fadf8a517bfcda209f5e1da04705db33e6989
juliehuynh9 <76454908+juliehuynh9@users.noreply.github.com>
2024-05-09 03:49:04 -0700

    updated to print out analytics

--- main.py
+++ main.py
@@ -9,6 +9,16 @@ def start_reading():
 
     reader.build_index(data_path)
 
+    # gather analytics
+    num_indexed_doc = reader.num_indexed_doc
+    num_unique_words = reader.num_unique_words
+    total_index_size = reader.total_index_size
+
+    print("Analytics:")
+    print("Number of Indexed Documents:", num_indexed_doc)
+    print("Number of Unique Tokens:", num_unique_words)
+    print("Total Index Size (KB):", total_index_size)
+
 
 if __name__ == "__main__":
     start_reading()
