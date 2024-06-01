cb96864bff6deff9315fdbc5336679b3abbbe47d
Stanley Lu <stanlehl@uci.edu>
2024-05-31 19:31:35 -0700

    Created separate py files for creating index and running search engine.

---
+++ createIndex.py
@@ -0,0 +1,17 @@
+import sys
+
+import FileReader as f
+
+def run(path):
+    reader = f.FileReader()
+    reader.build_final_index(path)
+    reader.create_positions_index('index.json', "positions_index.json")
+    reader.convert_index_to_txt('index.json')
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print('Usage: python main.py path_to_zip_folder')
+        sys.exit(1)
+    path = sys.argv[1]
+    run(path)
--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 import sys
 
-import FileReader as f
 import SearchEngine as s
 
 def run(path, path2, path3):
-    # reader = f.FileReader()
-    # reader.build_final_index(path)
-    # reader.create_positions_index(path, "positions_index.json")
-    # reader.convert_index_to_txt(path)
     engine = s.SearchEngine(path, path2, path3)
     engine.run()
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print('Usage: python main.py path_to_zip_folder')
+        print('Usage: python main.py index.txt doc_info.json positions_index.json')
         sys.exit(1)
     path = sys.argv[1]
     path2 = sys.argv[2]
